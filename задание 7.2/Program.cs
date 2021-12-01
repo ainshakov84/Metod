0034194b65688f2ff027b59cdcf545bc03b2d0be
Александр Иншаков <a.inshakov84@gmail.com>
2021-12-01 16:05:16 +0300

    Добавьте файлы проекта.

---
+++ задание 7.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace задание_7._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            double V, S;
+            KUB(a, out V, out S);
+            Console.Write("Объем куба =");
+            Console.WriteLine(V);
+            Console.Write("Площадь ребр куба =");
+            Console.WriteLine(S);
+
+            Console.ReadKey();
+        }
+        static void KUB(int a, out double V, out double S)
+        {
+            V = Math.Pow(a, 3);
+            S = 6 * Math.Pow(a, 2);
+        }
+    }
+}
+
